5f40c3773f356b62dbe14564ee638aed2f10bfad
wanyu <1073485249@qq.com>
2020-03-17 22:06:01 +0800

    <notes>:修改启动错误

--- springboot-learn-rabbitmq-demo/src/main/java/com/wanyu/springboot/learn/rabbitmq/demo/controller/SendMessageController.java
+++ springboot-learn-rabbitmq-demo/src/main/java/com/wanyu/springboot/learn/rabbitmq/demo/controller/SendMessageController.java
@@ -1,6 +1,7 @@
 package com.wanyu.springboot.learn.rabbitmq.demo.controller;
 
 import com.wanyu.springboot.learn.rabbitmq.demo.util.ConstantUtil;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -56,7 +57,8 @@ public class SendMessageController {
         map.put("messageId",messageId);
         map.put("messageData",messageData);
         map.put("createTime",createTime);
-        rabbitTemplate.convertAndSend(ConstantUtil.TOPIC_EXCHANGE_NAME,ConstantUtil.TOPIC_ROUTING_KEY_A,map);
+        rabbitTemplate.convertAndSend(ConstantUtil.TOPIC_EXCHANGE_NAME,
+                "boot_topic.xxx.yyy",map);
         return "ok";
     }
 
@@ -70,7 +72,7 @@ public class SendMessageController {
         map.put("messageId",messageId);
         map.put("messageData",messageData);
         map.put("createTime",createTime);
-        rabbitTemplate.convertAndSend(ConstantUtil.TOPIC_EXCHANGE_NAME,ConstantUtil.TOPIC_ROUTING_KEY_B,map);
+        rabbitTemplate.convertAndSend(ConstantUtil.TOPIC_EXCHANGE_NAME,"boot_topic.xxx",map);
         return "ok";
     }
 }
