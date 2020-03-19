5fd7d0d01cdbb07f6f925f026f9f0b0ececf6516
wanyu <1073485249@qq.com>
2020-03-19 16:18:13 +0800

    <notes>:创建整合redis的项目

---
+++ springboot-learn-redis-demo/src/test/java/com/wanyu/springboot/learn/redis/demo/SpringbootLearnRedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanyu.springboot.learn.redis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearnRedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
