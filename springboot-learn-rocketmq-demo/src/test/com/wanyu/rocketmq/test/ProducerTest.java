0b75f4169be68423aaec6a3ca3b8b4e4a180992a
wanyu <1073485249@qq.com>
2021-01-08 15:25:14 +0800

    新增Springboot整合RocketMQ

---
+++ springboot-learn-rocketmq-demo/src/test/com/wanyu/rocketmq/test/ProducerTest.java
@@ -0,0 +1,28 @@
+package com.wanyu.rocketmq.test;
+
+import com.wanyu.springboot.learn.rocketmq.demo.RocketMQDemoApplication;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author wan
+ * @version 1.0
+ * @date 2021/1/8 9:53
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {RocketMQDemoApplication.class})
+public class ProducerTest {
+
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @Test
+    public void testSendMessage(){
+        rocketMQTemplate.convertAndSend("springboot-rocketmq","Hello Springboot RocketMQ");
+    }
+}
