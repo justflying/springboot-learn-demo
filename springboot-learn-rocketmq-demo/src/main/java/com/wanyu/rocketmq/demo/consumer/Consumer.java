25e014544a8c2214dfe31d8157a44d67f1b2ccbb
wanyu <1073485249@qq.com>
2021-01-27 14:45:45 +0800

    添加消费者

--- springboot-learn-rocketmq-demo/src/main/java/com/wanyu/rocketmq/demo/consumer/Consumer.java
+++ springboot-learn-rocketmq-demo/src/main/java/com/wanyu/rocketmq/demo/consumer/Consumer.java
@@ -1,9 +1,20 @@
 package com.wanyu.rocketmq.demo.consumer;
 
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
 /**
  * @author wan
  * @version 1.0
  * @date 2021/1/27 10:34
  */
-public class Consumer {
+@Component
+@RocketMQMessageListener(consumerGroup = "consumer-group",topic = "springboot-rocketmq")
+public class Consumer implements RocketMQListener<String> {
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println(" 接收的消息是： " +message);
+    }
 }
